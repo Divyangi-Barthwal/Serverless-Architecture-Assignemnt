c3c340cf89e089dd9e0203304da7b558bcd4ea66
Divyangi Barthwal <divyangibarthwal@gmail.com>
2025-10-16 01:54:58 +0530

    Add files via upload

---
+++ S3.py
@@ -0,0 +1,13 @@
+import boto3
+from datetime import datetime, timezone, timedelta
+
+def lambda_handler(event, context):
+    bucket = 'your-bucket-name'
+    s3 = boto3.client('s3')
+    objs = s3.list_objects_v2(Bucket=bucket).get('Contents', [])
+    
+    for obj in objs:
+        age = datetime.now(timezone.utc) - obj['LastModified']
+        if age.days > 30:
+            s3.delete_object(Bucket=bucket, Key=obj['Key'])
+            print(f"Deleted: {obj['Key']}")
